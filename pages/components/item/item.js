93db4f214337f07e21c9cd9d1bec39298ec6878b
rongliu <rongliu@thoughtworks.com>
2018-07-03 09:45:24 +0800

    add item component.

---
+++ pages/components/item/item.js
@@ -0,0 +1,21 @@
+Component({
+  properties: {
+    item: {
+      type: {
+        index: {
+          type: Number
+        },
+        value: {
+          type: String
+        },
+        active: {
+          type: Boolean
+        },
+      }
+    }
+  },
+  methods: {
+    deleteItem: function() {
+    }
+  }
+})
