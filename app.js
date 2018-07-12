6f7043e0c8b87b17c674c877faa10b518b5d7d7b
rongliu <rongliu@thoughtworks.com>
2018-07-12 17:33:26 +0800

    add login logic.

--- app.js
+++ app.js
@@ -2,39 +2,39 @@ import { config } from './utils/constants.js'
 
 App({
   onLaunch: function () {
-    wx.login({
-      success: res => {
-        const userCode = res.code
-        if (userCode) {
-          wx.request({
-            url: `${config.host}/api/user/${userCode}`,
-            success: (res) => {
-              this.globalData.openId = res.data.openId
-              wx.redirectTo({
-                url: '../todo/todo',
-              })
-            }
-          })
-        }
-      }
-    })
-    // 获取用户信息
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+          this.getUserInfo()
+          this.getOpenIdAndRedirect()
+        }
+      }
+    })
+  },
+  getOpenIdAndRedirect: function () {
+    wx.login({
+      success: res => {
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+        wx.request({
+          url: `${config.host}/api/user/${res.code}`,
+          success: (res) => {
+            this.globalData.openId = res.data.openId
+  
+            wx.redirectTo({
+              url: '../todo/todo',
+            })
+          }
+        })
+      }
+    })
+  },
+  getUserInfo: function () {
+    wx.getUserInfo({
+      success: res => {
+        this.globalData.userInfo = res.userInfo
+
+        if (this.userInfoReadyCallback) {
+          this.userInfoReadyCallback(res)
         }
       }
     })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,8 @@
+import { config } from '../../utils/constants.js'
 const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
@@ -14,8 +14,6 @@ Page({
         hasUserInfo: true
       })
     } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
@@ -23,7 +21,6 @@ Page({
         })
       }
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
         success: res => {
           app.globalData.userInfo = res.userInfo
@@ -36,11 +33,27 @@ Page({
     }
   },
   getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    if (e.detail.userInfo) {
+      app.globalData.userInfo = e.detail.userInfo
+      wx.login({
+        success: res => {
+
+          wx.request({
+            url: `${config.host}/api/user/${res.code}`,
+            success: (res) => {
+              app.globalData.openId = res.data.openId
+
+              wx.redirectTo({
+                url: '../todo/todo',
+              })
+            }
+          })
+        }
+      })
+      this.setData({
+        userInfo: e.detail.userInfo,
+        hasUserInfo: true
+      })
+    }
   }
 })
